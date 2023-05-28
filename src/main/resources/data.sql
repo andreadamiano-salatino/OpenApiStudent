8946036e4efbf00cd76d807d4c48512764a9dc73
andreadamiano-salatino <andreadamiano.salatino@gmail.com>
2023-05-28 19:34:53 +0200

    Bug fix
    Minor bug fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO STUDENTS (id, serial_number, name, surname, date_of_birth, cf)
-VALUES(1, '123456789', 'Mario', 'Rossi', '1980-01-01', 'RSSMRA80A01H501U');
+VALUES(1, '000000001', 'Mario', 'Rossi', '1980-01-01', 'RSSMRA80A01H501U');
 INSERT INTO STUDENTS (id, serial_number, name, surname, date_of_birth, cf)
-VALUES(2, '123456789', 'Paolo', 'Bianchi', '1980-02-02', 'BNCPLA80B02H501D');
+VALUES(2, '000000002', 'Paolo', 'Bianchi', '1980-02-02', 'BNCPLA80B02H501D');
 INSERT INTO STUDENTS (id, serial_number, name, surname, date_of_birth, cf)
-VALUES(3, '123456789', 'Maria', 'Rossi', '1980-03-03', 'MRARSS90C03H501G');
+VALUES(3, '000000003', 'Maria', 'Rossi', '1980-03-03', 'MRARSS90C03H501G');
 
 CREATE SEQUENCE STUDENTS_SEQUENCE_ID START WITH (select max(id) + 1 from STUDENTS);
